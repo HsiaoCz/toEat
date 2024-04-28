55154a24bc026cd4ddd2200a9623a5c6b7ca3971
HsiaoCz <984274788@qq.com>
2024-04-29 07:45:56 +0800

    holy fork

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,62 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 
-const restaurantList: any = ref([])
-const newRestaurantName = ref('')
+/**
+ * Restaurants
+ * 
+ * Name - string
+ * Address - string
+ * States - string (e.g. "Want to Try" | "Must Try")
+ * Dishes - array of Dish objects
+ */
+
+interface Restaurant {
+  name?: string
+  // address?: string
+  status?: RestaurantStatus
+  dishes?: Dish[]
+}
+
+const ALL_STATUS = ["Want to Try", "Recommend", "Must Try"]
+
+type RestaurantStatus = "Want to Try" | "Recommended" | "Do Not Recommend" | "Must try"
+
+const restaurantList = ref<Restaurant[]>([])
+const newRestaurant = ref<Restaurant>({})
+const statusList = [
+  "Want to Try",
+  "Recommended",
+  "Do Not Recommend",
+  "Must try"
+]
 
 function addRestaurant() {
-  restaurantList.value.push(newRestaurantName.value)
+  restaurantList.value.push({
+    name: newRestaurant.value.name,
+    // address: "",
+    status: "Want to Try",
+    dishes: []
+  })
 }
 </script>
 
 <template>
   <main>
+    <pre>{{ newRestaurant }}</pre>
     <!-- create a form that allows users to add a restourant to a list -->
     <form @submit.prevent="addRestaurant">
-      <label for="restaurant-name">Restaurant Name</label>
-      <input id="restaurant-name" v-model="newRestaurantName" type="text" />
+      <div>
+        <label for="restaurant-name">Restaurant Name</label>
+        <input id="restaurant-name" v-model="newRestaurant.name" type="text" />
+      </div>
+      <!-- <div>
+        <label for="restaurant-address">Restaurant Address</label>
+        <input id="restaurant-address" v-model="newRestaurant.address" type="text" />
+      </div> -->
+      <div>
+        <label for="restaurant-status">Restaurant Status</label>
+        <input id="restaurant-status" v-model="newRestaurant.status" type="text" />
+      </div>
       <button type="submit">Add Restaurant</button>
     </form>
     <ul>
